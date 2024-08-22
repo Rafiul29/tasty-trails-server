6810ceccc2e78aa9da0f0e0c5023829a9c022212
Rafiul29 <rafiulislam4472@gmail.com>
2024-08-23 01:12:02 +0600

    create order api fix

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError
 
 from .models import DeliveryAddress,Order,OrderItem
+from carts.models import CartItem
+from account.models import User
+import uuid
 
-
-class OrderSerializer(serializers.ModelSerializer):
+class DeliveryAddressSerializer(serializers.ModelSerializer):
   class Meta:
-    model=Order
+    model=DeliveryAddress
     fields='__all__'
 
 
@@ -16,8 +19,54 @@ class OrderItemSerializer(serializers.ModelSerializer):
     fields='__all__'
 
 
-class DeliveryAddressSerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
+  delivery_address = DeliveryAddressSerializer()
   class Meta:
-    model=DeliveryAddress
+    model=Order
     fields='__all__'
+
+  def create(self, validated_data):
+    print(validated_data)
+    delivery_address_data = validated_data.pop('delivery_address')
+    user = self.context['request'].user
+  
+    active_cart_items = CartItem.objects.filter(user=user, is_active=True)
+
+    if not active_cart_items.exists():
+        raise ValidationError("No active cart items found for the user.")
+    
+    order_total = sum(item.quantity * item.menu_item.price for item in active_cart_items)
+
+    tax=(2*order_total)/100
+    order_total+=tax
+    order_number=str(uuid.uuid4())[:10].replace('-', '').upper()
+
+    # //create  delivery adderess
+    delivery_address = DeliveryAddress.objects.create(**delivery_address_data)
+
+    # create order
+    order = Order.objects.create(delivery_address=delivery_address,order_number=order_number,order_total=order_total,**validated_data)
+
+    # create order item
+    for item in active_cart_items:
+        OrderItem.objects.create(
+                user=user,
+                order=order,
+                menu_item=item.menu_item,
+                quantity=item.quantity,
+                price=item.menu_item.price
+     )
+
+    active_cart_items.delete()
+    return order
+  
+  def update(self, instance, validated_data):
+        # Only update the status field
+        status = validated_data.get('status', None)
+        if status is not None:
+            instance.status = status
+            instance.save()
+        return instance
+
+
 
--- orders/views.py
+++ orders/views.py
@@ -5,42 +5,18 @@ from rest_framework.permissions import IsAuthenticated
 from carts.models import CartItem 
 from .models import Order,OrderItem,DeliveryAddress
 from .serializers import OrderSerializer,OrderItemSerializer,DeliveryAddressSerializer
-import uuid
 
-# Create your views here.
+
 class OrderViewSet(viewsets.ModelViewSet):
-  queryset=Order.objects.all()
-  serializer_class=OrderSerializer
-  permission_classes = [IsAuthenticated]
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
-  def get_queryset(self):
+    def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
-  def perform_create(self, serializer):
-        user = self.request.user
-        delivery_address = serializer.validated_data['delivery_address']
-       
-        active_cart_items = CartItem.objects.filter(user=user, is_active=True)
-        order_total = sum(item.quantity * item.menu_item.price for item in active_cart_items)
-
-        tax=(2*order_total)/100
-        order_total+=tax
-        
-        order_number=str(uuid.uuid4())[:10].replace('-', '').upper()
-
-        # Save the order with the calculated order total
-        order = serializer.save(user=user, order_total=order_total,delivery_address=delivery_address,order_number=order_number)
-
-        for item in active_cart_items:
-            OrderItem.objects.create(
-                user=user,
-                order=order,
-                menu_item=item.menu_item,
-                quantity=item.quantity,
-                price=item.menu_item.price
-            )
-
-        active_cart_items.delete()
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class OrderItemViewSet(viewsets.ModelViewSet):
   queryset=OrderItem.objects.all()
